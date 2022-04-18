3aeee8cfec035fd876fb107cbc82d49b50af3ed7
Matt Chisholm <mathesonchisholm@hotmail.com>
2022-04-18 14:59:28 -0700

    setup for axios req

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,20 @@ import React, { Component } from "react";
 export default class SearchBar extends Component {
   state = { term: "" };
 
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value })
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    // TODO: Make sure we call prop callback from App Component
+  }
+
   render() {
     return (
       <div className="ui segment search-bar">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Video Search</label>
             <input type="text" value={this.state.term} onChange={this.onInputChange} />
